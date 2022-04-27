98f9b3b996289e2eae2a88c3630f52b1e7f17cbf
charlypg <charlypecqueuxguezenec@gmail.com>
2022-04-27 13:17:59 +0200

    created balzax modules

---
+++ balzax/balls_base.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import jax
+import jax.numpy as jnp
+from functools import partial
+
+from structures import Ball, State
+from structures import create_wall, get_ball_colliders, pen_res_functions_w
+from structures import update_agent_pos, update_bb, update_bw
+from structures import sample_ball_uniform_pos
+from structures import out
+
+from image_generation import balls_to_one_image
+
+
+def done_timestep(state: State, timestep, max_timestep = 10000) -> bool:
+    return out(state.ball) | (timestep >= max_timestep)
+
+class BallsBase:
+    def __init__(self, obs_type : str = 'position'): 
+        self.num_balls = 4
+        self.x_limit = 1.
+        self.y_limit = 1.
+        self.init_radius = (1. + jnp.arange(self.num_balls, dtype=jnp.float32))
+        self.init_radius *= (min(self.x_limit, self.y_limit) / 20.)
+        
+        self.colors = 255*jnp.ones((self.num_balls,), dtype=jnp.int32)
+        self.image_dim = 224
+        
+        self.ball_colliders = get_ball_colliders(self.num_balls)
+        self.solve_bb_collisions = partial(update_bb, 
+                                           ball_colliders=self.ball_colliders)
+        
+        self.walls = []
+        self.walls.append(create_wall(start=jnp.array([0., 
+                                                       0.]),
+                                      end=jnp.array([self.x_limit, 
+                                                     0.])))
+        self.walls.append(create_wall(start=jnp.array([0., 
+                                                       0.]),
+                                      end=jnp.array([0., 
+                                                     self.y_limit])))
+        self.walls.append(create_wall(start=jnp.array([self.x_limit, 
+                                                       0.]),
+                                      end=jnp.array([self.x_limit, 
+                                                     self.y_limit])))
+        self.walls.append(create_wall(start=jnp.array([0., 
+                                                       self.y_limit]),
+                                      end=jnp.array([self.x_limit, 
+                                                     self.y_limit])))
+        
+        self.solve_bw_collisions = partial(update_bw, 
+                                           pen_res_fcts=pen_res_functions_w(self.walls))
+        
+        
+        self.obs_type = obs_type
+        self.obs_fcts = {
+                        'position': self.get_pos, 
+                        'image': self.get_image
+                         }
+        self.get_obs = self.obs_fcts.get(self.obs_type)
+    
+    def apply_update(self, 
+                     balls: Ball, 
+                     agent_pos_var: jnp.ndarray = jnp.zeros((2,))) -> Ball:
+        """Update positions of balls"""
+        new_balls = update_agent_pos(balls, agent_pos_var)
+        new_balls = self.solve_bb_collisions(new_balls)
+        new_balls = self.solve_bw_collisions(new_balls)
+        return new_balls
+    
+    def reset_base(self, key):
+        """Resets the environment : new ball positions and new random key"""
+        new_balls = sample_ball_uniform_pos(key, 
+                                            self.num_balls, 
+                                            self.x_limit, 
+                                            self.y_limit, 
+                                            self.init_radius)
+        for _ in range(10):
+            new_balls = self.solve_bb_collisions(new_balls)
+            new_balls = self.solve_bw_collisions(new_balls)
+        new_key = jax.random.split(key)[0]
+        return new_balls, new_key
+    
+    # Static method : precise ?
+    def get_pos(self, balls: Ball) -> jnp.ndarray:
+        """Returns positions from balls"""
+        return balls.pos.flatten()
+    
+    def get_image(self, balls: Ball) -> jnp.ndarray:
+        """Returns an image from balls"""
+        return balls_to_one_image(balls,
+                                  self.colors,
+                                  self.image_dim)
+    
+    def get_dpos(self, action: jnp.ndarray) -> jnp.ndarray:
+        """Returns a position variation from a velocity angle."""
+        angle = jnp.pi * action
+        return 0.02 * jnp.array([jnp.cos(angle), jnp.sin(angle)])
+    
+    def common_step(self, 
+                    state: State, 
+                    action: jnp.ndarray) -> State:
+        """Performs an MDP step : s_t + a_t -> s_{t+1}"""
+        dpos = self.get_dpos(action)
+        new_balls = self.apply_update(state.ball, dpos)
+        new_obs = self.get_obs(new_balls)
+        new_state = State(ball=new_balls, 
+                          obs=new_obs)
+        return new_state
+    
+    def compute_done(self, state: State, timestep) -> bool:
+        """Returns whether the state is terminal or not"""
+        return done_timestep(state, timestep)
---
+++ balzax/image_generation.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import jax
+import jax.numpy as jnp
+from functools import partial
+
+from structures import Ball
+
+def scale_ball(ball: Ball, scale) -> Ball:
+    """Performs homothety"""
+    return Ball(pos=scale*ball.pos, radius=scale*ball.radius)
+
+def isin_ball(y: jnp.ndarray, x: jnp.ndarray, ball: Ball):
+    """(x,y) in ball ?"""
+    sq = jnp.array([x, y])
+    sq = sq - ball.pos
+    sq = sq*sq
+    return (jnp.sum(sq) <= ball.radius*ball.radius)
+
+def isin_ball_vect_y(x: jnp.ndarray, y_vect: jnp.ndarray, ball: Ball):
+    """Batch version over y axis of isin_ball"""
+    return jax.vmap(partial(isin_ball, x=x, ball=ball))(y_vect)
+
+def isin_ball_mat(x_vect: jnp.ndarray, y_vect: jnp.ndarray, ball: Ball):
+    """Batch version over x axis of isin_ball_vect_y in order to obtain a 
+    boolean image."""
+    return jax.vmap(partial(isin_ball_vect_y, y_vect=y_vect, ball=ball))(x_vect)
+
+def ball_to_imbool(reg_ball: Ball,  
+                  image_dim: int = 224):
+    """Boolean image of a ball"""
+    x_vect = jnp.arange(image_dim)
+    y_vect = jnp.arange(image_dim)
+    image_ball = scale_ball(reg_ball, image_dim)
+    return isin_ball_mat(x_vect, y_vect, image_ball)
+
+def ball_to_image(reg_ball: Ball,
+                  color: jnp.ndarray = jnp.array([255, 0, 0], dtype=jnp.int32),
+                  image_dim: int = 224):
+    """Returns an image of a colored ball."""
+    mask = jnp.expand_dims(ball_to_imbool(reg_ball, image_dim), 
+                              axis=-1)
+    return mask * color
+
+def balls_to_images(reg_ball: Ball,
+                    color: jnp.ndarray,
+                    image_dim: int = 224):
+    """Returns a batch of colored balls"""
+    return jax.vmap(ball_to_image, in_axes=[0, 0, None])(reg_ball, color, image_dim)
+
+def balls_to_one_image(reg_ball: Ball,
+                       color: jnp.ndarray,
+                       image_dim: int = 224):
+    """Returns one image with all batched balls"""
+    return jnp.clip(jnp.sum(balls_to_images(reg_ball, color, image_dim), axis=0),
+                    0,
+                    255)
+    
