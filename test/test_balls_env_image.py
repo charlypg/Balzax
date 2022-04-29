334ca10fafd9fb4c46849e06a881fbbf15e5bf5d
charlypg <charlypecqueuxguezenec@gmail.com>
2022-04-29 18:12:07 +0200

    test vector BallsEnv

---
+++ test/test_balls_env_image.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+from time import time
+ 
+from balzax.balls_env import BallsEnv
+
+print("TEST 1 : BallsEnv(obs_type='image')")
+print()
+ 
+env = BallsEnv(obs_type='image', max_timestep=500)
+jit_env_reset_done = jax.jit(env.reset_done)
+jit_env_reset = jax.jit(env.reset) # env.reset
+jit_env_step = jax.jit(env.step) # env.step
+ 
+key = jax.random.PRNGKey(0)
+nb_iter = 10_000
+pulse = 2*jnp.pi/200
+image_list = []
+ 
+t0 = time()
+env_state = jit_env_reset(key)
+print("Time to reset (jit+exec) : {}s".format(time()-t0))
+print("State of the environment : Timestep 0")
+print(env_state)
+print()
+
+t0 = time()
+env_state = jit_env_reset_done(env_state)
+print("Time to reset_done (jit+exec) : {}s".format(time()-t0))
+print("State of the environment : Timestep 0")
+print(env_state)
+print()
+ 
+image_list.append(env_state.obs)
+ 
+ 
+t0 = time()
+env_state = jit_env_step(env_state, jnp.array(0.))
+print("Time of first step (jit+exec) : {}s".format(time()-t0))
+print("State of the environment : Timestep 1")
+print(env_state)
+print()
+ 
+image_list.append(env_state.obs)
+ 
+t0 = time()
+for i in range(nb_iter):
+    env_state = jit_env_step(env_state, jnp.sin(pulse*i))
+    env_state = jit_env_reset_done(env_state)
+    #image_list.append(env_state.obs)
+     
+print("Rollout of {0} iterations (compiled step and reset_done) : {1}".format(nb_iter, 
+                                                                          time()-t0))
+print()
+"""
+for i, image in enumerate(image_list):
+    plt.figure(i)
+    plt.title('Timestep {}'.format(i))
+    plt.imshow(image, origin='lower')"""
--- test/test_balls_env_vectorization.py
+++ test/test_balls_env_vectorization.py
@@ -8,13 +8,14 @@ import matplotlib.pyplot as plt
 
 from time import time
 
-from balzax import BallsEnv
+from balzax.balls_env import BallsEnv
 
 OBS_TYPE = 'image'
+MAX_TIMESTEPS = 3
 SEED = 0
 NUM_ENV = 3
 
-NB_ITER = 100
+NB_ITER = 21
 
 ACTION_0 = jnp.zeros((NUM_ENV,))
 ACTION_1 = jnp.ones((NUM_ENV,))/2.
@@ -24,8 +25,9 @@ keys = jax.random.split(key, num=NUM_ENV)
 
 #print(keys)
 
-env = BallsEnv(obs_type=OBS_TYPE)
+env = BallsEnv(obs_type=OBS_TYPE, max_timestep=MAX_TIMESTEPS)
 
+vmap_env_reset_done = jax.jit(jax.vmap(env.reset_done)) 
 vmap_env_reset = jax.jit(jax.vmap(env.reset))  # jax.vmap(env.reset)
 vmap_env_step = jax.jit(jax.vmap(env.step))  # jax.vmap(env.step)
 
@@ -39,40 +41,46 @@ print()
 
 t0 = time()
 env_states = vmap_env_reset(keys)
-print("Time du reset (jit+exec) : {}".format(time()-t0))
+print("Time of reset (jit+exec) : {}".format(time()-t0))
+print()
+
+t0 = time()
+env_states = vmap_env_reset_done(env_states)
+print("Time of reset_done (jit+exec) : {}".format(time()-t0))
 print()
 
 #print(env_states)
 
-observations = env_states.state.obs
+observations = env_states.obs
 print("observations : {}".format(observations.shape))
 print()
 
 t0 = time()
 env_states = vmap_env_reset(keys)
-print("Time du reset (second time exec) : {}".format(time()-t0))
+print("Time of reset (second time exec) : {}".format(time()-t0))
 print()
 
-obs_list.append(env_states.state.obs)
+obs_list.append(env_states.obs)
 
 t0 = time()
 env_states = vmap_env_step(env_states, ACTION_0)
 print("First step (jit+exec) : {}".format(time()-t0))
 print()
 
-obs_list.append(env_states.state.obs)
+obs_list.append(env_states.obs)
 
 t0 = time()
 env_states = vmap_env_step(env_states, ACTION_1)
 print("Second step (exec) : {}".format(time()-t0))
 print()
 
-obs_list.append(env_states.state.obs)
+obs_list.append(env_states.obs)
 
 t0 = time()
 for _ in range(NB_ITER):
     env_states = vmap_env_step(env_states, ACTION_1)
-    obs_list.append(env_states.state.obs)
+    env_states = vmap_env_reset_done(env_states)
+    obs_list.append(env_states.obs)
 print("{0} iterations in {1}s".format(NB_ITER, time()-t0))
 print()
 
@@ -80,10 +88,11 @@ pulse = 2*jnp.pi / NB_ITER * jnp.ones((NUM_ENV,))
 t0 = time()
 for i in range(NB_ITER):
     env_states = vmap_env_step(env_states, jnp.sin(pulse*i))
-    obs_list.append(env_states.state.obs)
+    env_states = vmap_env_reset_done(env_states)
+    obs_list.append(env_states.obs)
 print("{0} iterations in {1}s".format(NB_ITER, time()-t0))
 print()
-"""
+
 for i, obs in enumerate(obs_list):
     plt.figure(i)
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
@@ -93,4 +102,4 @@ for i, obs in enumerate(obs_list):
     ax2.set_title('Env 1')
     ax2.imshow(obs[1], origin='lower')
     ax3.set_title('Env 2')
-    ax3.imshow(obs[2], origin='lower')"""
+    ax3.imshow(obs[2], origin='lower')
