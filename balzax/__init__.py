212154549f2d3c0bd51d9afd52cf36f5bfd983c1
charlypg <charlypecqueuxguezenec@gmail.com>
2022-05-04 17:57:34 +0200

    Added max_timespteps_episode=None in specs

--- balzax/__init__.py
+++ balzax/__init__.py
@@ -5,11 +5,13 @@ from balzax.balls_env_goal import BallsEnvGoal
 gym.register(
     id="GymBallsEnvGoal-position-v0",
     entry_point="balzax.gym_balls_env_goal:GymBallsEnvGoal",
+    max_episode_steps=None,
     kwargs={'obs_type': 'position'}
 )
 
 gym.register(
     id="GymBallsEnvGoal-image-v0",
     entry_point="balzax.gym_balls_env_goal:GymBallsEnvGoal",
+    max_episode_steps=None,
     kwargs={'obs_type': 'image'}
 )
