d3a42ddf982b4078053e9ae27c335fa8297ddfb4
Charly PECQUEUX--GUÉZÉNEC <91844362+charlypg@users.noreply.github.com>
2022-05-05 21:41:42 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name="balzax",
-      version="0.0.1",
+      version="0.0.2",
       author="Charly Pecqueux--Guezenec",
       description="Game environment for reinforcement learning and open-ended learning composed of balls moving other balls and coded in JAX.",
       url="https://github.com/charlypg/Balzax",
