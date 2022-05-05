ef37b317558983ad65e0cebaa94e2ae55f094def
Charly PECQUEUX--GUÉZÉNEC <91844362+charlypg@users.noreply.github.com>
2022-05-05 16:35:11 +0200

    uncomment prints

--- balzax/wrapper.py
+++ balzax/wrapper.py
@@ -299,15 +299,15 @@ class GoalGymVecWrapper(GoalEnv):
         return jnpdict_to_onpdict(self.env_state.goalobs)
     
     def step(self, action : onp.ndarray):
-        #print("DEBUG : action shape : {0}".format(action.shape))
+        print("DEBUG : action shape : {0}".format(action.shape))
         self.env_state = self.step_be(self.env_state, jnp.array(action))
         goalobs = jnpdict_to_onpdict(self.env_state.goalobs)
-        #print("DEBUG : goal shape : {0}".format(goalobs.get('desired_goal').shape))
-        #print("DEBUG : obs shape : {0}".format(goalobs.get('observation').shape))
+        print("DEBUG : goal shape : {0}".format(goalobs.get('desired_goal').shape))
+        print("DEBUG : obs shape : {0}".format(goalobs.get('observation').shape))
         reward = onp.array(self.env_state.reward)
-        #print("DEBUG : reward shape : {0}".format(reward.shape))
+        print("DEBUG : reward shape : {0}".format(reward.shape))
         done = onp.array(self.env_state.done)
-        #print("DEBUG : done shape : {0}".format(done.shape))
+        print("DEBUG : done shape : {0}".format(done.shape))
         info = jnpdict_to_onpdict(self.env_state.metrics)
         return goalobs, reward, done, info
     
