ddac51fdd12dc1ac433af7ff38b6ea6ba042e7da
charlypg <charlypecqueuxguezenec@gmail.com>
2022-05-10 14:48:13 +0200

    Added random_reset_base and used in balls_base

--- balzax/balls_base.py
+++ balzax/balls_base.py
@@ -1,4 +1,3 @@
-import jax
 import jax.numpy as jnp
 from functools import partial
 
@@ -6,18 +5,23 @@ from balzax.structures import Ball
 from balzax.structures import create_wall, get_ball_colliders, pen_res_functions_w
 from balzax.structures import update_agent_pos, update_bb, update_bw
 from balzax.structures import out
-from balzax.optim_reset_base import sample_uniform_pos, solve_reset
-
+from balzax.random_reset_base import compute_r, random_reset
 from balzax.image_generation import balls_to_one_image
 
 
 class BallsBase:
     def __init__(self, obs_type : str = 'position'): 
         self.num_balls = 4
-        self.x_limit = 1.
-        self.y_limit = 1.
-        self.init_radius = (1. + jnp.arange(self.num_balls, dtype=jnp.float32))
-        self.init_radius *= (min(self.x_limit, self.y_limit) / 5 / self.num_balls)
+        self.L = 1.
+        self.x_limit = self.L 
+        self.y_limit = self.L 
+        self.epsilon = 1e-12
+        self.N_randsamp = 50
+        self.r_max = compute_r(epsilon=self.epsilon, 
+                               N=self.N_randsamp, 
+                               n=self.num_balls, 
+                               L=self.L)
+        self.init_radius = self.r_max * jnp.ones((self.num_balls,))
         
         self.colors = 255*jnp.ones((self.num_balls,), dtype=jnp.int32)
         self.image_dim = 224
@@ -72,18 +76,12 @@ class BallsBase:
     
     def reset_base(self, key):
         """Resets the game : new ball positions and new random key"""
-        pos_0 = sample_uniform_pos(key, 
-                                   self.num_balls, 
-                                   self.x_limit, 
-                                   self.y_limit, 
-                                   self.init_radius)
-        
-        pos, fun_val = solve_reset(pos_0.flatten(), self.init_radius)
-        
-        new_balls = Ball(pos=pos.reshape(self.init_radius.shape[0], 2),
-                         radius=self.init_radius)
-        
-        new_key = jax.random.split(key)[0]
+        new_balls, new_key = random_reset(key, 
+                                          num_balls=self.num_balls, 
+                                          x_limit=self.x_limit, 
+                                          y_limit=self.y_limit, 
+                                          radius=self.init_radius, 
+                                          N=self.N_randsamp)
         return new_balls, new_key
     
     def step_base(self, balls: Ball, action: jnp.ndarray) -> Ball:
---
+++ balzax/random_reset_base.py
@@ -0,0 +1,103 @@
+import numpy as onp
+import jax
+import jax.numpy as jnp
+from functools import partial
+
+from balzax.structures import Ball
+from balzax.structures import sample_ball_uniform_pos
+
+def f(p_M, n):
+    return onp.sqrt(p_M/(2*n*(n-1)))
+
+def lininv(x):
+    return x/(1+x)
+
+def compute_l(p_M, n, L):
+    return lininv(f(p_M, n))*L
+
+def compute_N(p_M, epsilon):
+    return 1 + int(onp.log(epsilon)/onp.log(p_M))
+
+def compute_p_M(epsilon, N):
+    return epsilon**(1/N)
+
+def compute_r(epsilon, N, n, L):
+    p_M = compute_p_M(epsilon, N)
+    return compute_l(p_M, n, L) / 2
+
+def sample_balls(keys, num_balls, x_limit, y_limit, radius):
+    return jax.vmap(partial(sample_ball_uniform_pos,
+                            num_balls=num_balls, 
+                            x_limit=x_limit,
+                            y_limit=y_limit,
+                            radius=radius))(keys)
+
+def elem_dist_cost(ball_1: Ball, ball_2: Ball):
+    r_part = (ball_1.radius + ball_2.radius)**2
+    d_part = jnp.sum((ball_1.pos - ball_2.pos)**2)
+    cost = jax.nn.relu(r_part - d_part)**2
+    return cost
+
+def line_dist_cost(ball_2: Ball, balls_1: Ball):
+    return jax.vmap(partial(elem_dist_cost, ball_2=ball_2))(balls_1)
+
+def squa_dist_cost(balls: Ball):
+    return jax.vmap(partial(line_dist_cost, balls_1=balls))(balls)
+
+def collision_cost(balls: Ball):
+    return jnp.sum(squa_dist_cost(balls))
+
+def constant_cost(balls: Ball):
+    return -16 * jnp.sum(balls.radius**4)
+
+@jax.vmap
+def subs_pos_rad(pos: jnp.ndarray, radius: jnp.ndarray):
+    return pos - radius
+
+@jax.vmap
+def add_m1_pos_rad(pos: jnp.ndarray, radius: jnp.ndarray):
+    return pos + radius - 1.
+
+def up_left_cost(balls: Ball):
+    terms = jax.nn.relu( - subs_pos_rad(balls.pos, balls.radius) )**2
+    return jnp.sum(terms)
+
+def low_right_cost(balls: Ball):
+    terms = jax.nn.relu(add_m1_pos_rad(balls.pos, balls.radius))**2
+    return jnp.sum(terms)
+    
+def cost(balls: Ball):
+    costs = jnp.array([collision_cost(balls),
+                       constant_cost(balls),
+                       up_left_cost(balls),
+                       low_right_cost(balls)])
+    return jnp.sum(costs)
+
+def costs(balls):
+    return jax.vmap(cost)(balls)
+
+def random_reset(key, num_balls, x_limit, y_limit, radius, N):
+    new_key = jax.random.split(key)[0]
+    keys = jax.random.split(key, num=N)
+    balls = sample_balls(keys, num_balls, x_limit, y_limit, radius)
+    cc = costs(balls)
+    i_min = jnp.argmin(cc)
+    ball_samp = Ball(pos=balls.pos[i_min], radius=balls.radius[i_min])
+    return ball_samp, new_key
+    
+def test_random_reset(nb_iter, num_balls, x_limit, y_limit, radius, N):
+    @jax.jit
+    def aux(key):
+        ball_samp, new_key = random_reset(key, 
+                                          num_balls, 
+                                          x_limit, 
+                                          y_limit, 
+                                          radius, 
+                                          N)
+        return ball_samp, new_key
+    key = jax.random.PRNGKey(0)
+    cost_list = []
+    for _ in range(nb_iter):
+        ball_samp, key = aux(key)
+        cost_list.append(cost(ball_samp))
+    return cost_list
