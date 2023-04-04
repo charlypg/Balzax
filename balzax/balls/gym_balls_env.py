c5b0fa010a3a95a3d65a91682fea56c41570ed92
charlypg <pecqueuxguezenec@isir.upmc.fr>
2023-04-04 13:53:26 +0200

    update balzax architecture: gym balls wrappers

--- balzax/balls/gym_balls_env.py
+++ balzax/balls/gym_balls_env.py
@@ -1,55 +1,15 @@
 import gym
 from typing import Optional
 
-from balzax.wrapper import GymWrapper, GymVecWrapper
+from balzax.wrapper import GymVecWrapper
 from balzax.balls.balls_env import BallsEnv
-
-
-def gym_single_balls_env_factory(
-    name: str,
-    obs_type: str = "position",
-    num_balls: int = 4,
-    max_episode_steps: int = 500,
-    seed: int = 0,
-    backend: Optional[str] = None,
-):
-    gym.register(
-        id=name,
-        entry_point="balzax.balls.gym_balls_env:GymSingleBallsEnv",
-        max_episode_steps=None,
-        kwargs={
-            "obs_type": obs_type,
-            "num_balls": num_balls,
-            "max_episode_steps": max_episode_steps,
-            "seed": seed,
-            "backend": backend,
-        },
-    )
-
-
-class GymSingleBallsEnv(GymWrapper):
-    """Gym environment wrapping a BallsEnv environment"""
-
-    def __init__(
-        self,
-        obs_type: str = "position",
-        num_balls: int = 4,
-        max_episode_steps: int = 500,
-        seed: int = 0,
-        backend: Optional[str] = None,
-    ):
-        env = BallsEnv(
-            obs_type=obs_type, num_balls=num_balls, max_episode_steps=max_episode_steps
-        )
-        super().__init__(env=env, seed=seed, backend=backend)
-        self.max_episode_steps = max_episode_steps
 
 
 def gym_balls_env_factory(
     name: str,
     obs_type: str = "position",
     num_balls: int = 4,
-    max_episode_steps: int = 500,
+    max_episode_steps: int = 300,
     num_envs: int = 1,
     seed: int = 0,
     backend: Optional[str] = None,
@@ -76,7 +36,7 @@ class GymBallsEnv(GymVecWrapper):
         self,
         obs_type: str = "position",
         num_balls: int = 4,
-        max_episode_steps: int = 500,
+        max_episode_steps: int = 300,
         num_envs: int = 1,
         seed: int = 0,
         backend: Optional[str] = None,
